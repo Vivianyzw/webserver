74cf50c2def42945143bce8d52e4e8d026ed51ac
vivian <vivianziweiyang@gmail.com>
2020-03-30 12:57:50 +0800

    删除错误文件

--- FOTS-master/utils/common_str.py
+++
@@ -1,5 +0,0 @@
-number = '0123456789'
-alphabet = 'qwertyuiopasdfghjklzxcvbnm' + 'QWERTYUIOPASDFGHJKLZXCVBNM'
-alphabet_and_number = alphabet + number
-custom_1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.'
-number_and_dot = number + '.'
